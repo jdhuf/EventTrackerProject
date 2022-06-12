d0a25e41ee38149abd4e21169e04005bec5d1d9b
jdhuf <91219279+jdhuf@users.noreply.github.com>
2022-06-11 20:42:52 -0600

    added find one exercise by id functionality

--- ExerciseTrackerREST/src/main/resources/static/js/script.js
+++ ExerciseTrackerREST/src/main/resources/static/js/script.js
@@ -3,7 +3,16 @@ window.addEventListener('load', function(e){
 });
 
 function init(){
+	document.exerciseForm.findExerciseButton.addEventListener('click', function(event) {
+		event.preventDefault();
+		let exerciseId = document.exerciseForm.exerciseId.value;
+		if (!isNaN(exerciseId) && exerciseId > 0) {
+			getExercise(exerciseId);
+		}
+	});
 	loadExerciseList();
+
+	
 }
 
 function loadExerciseList(){
@@ -52,4 +61,46 @@ function displayExerciseList(exerciseList) {
 		td.textContent = exercise.repetitions;
 		tr.appendChild(td);
 	}
+}
+
+
+
+function getExercise(exerciseId) {
+	
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET', 'api/exercise/' + exerciseId);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200 && xhr.responseText) {
+				let exercise = JSON.parse(xhr.responseText);
+				displayExercise(exercise);
+
+			}
+			else {
+				displayError('Exercise not found.');
+
+			}
+
+		}
+	}
+
+	xhr.send();
+}
+
+function displayExercise(exercise) {
+	let dataDiv = document.getElementById('exerciseData');
+	dataDiv.textContent = '';
+
+
+	let listOfExerciseData = ["Id: " + exercise.id, "Name: " + exercise.name, "Distance: " + exercise.distance, "Duration: " + exercise.duration, "Repetitions: " + exercise.repetitions];
+	let ul = document.createElement('ul');
+	listOfExerciseData.forEach(function(value) {
+
+		let li = document.createElement('li');
+		li.textContent = value;
+		ul.appendChild(li);
+
+	});
+	dataDiv.appendChild(ul);
+
 }
