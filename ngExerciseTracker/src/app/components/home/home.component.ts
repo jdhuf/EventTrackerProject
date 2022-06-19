e46b69bba652db8e3c449797dfa8fdfc93d1b9b9
jdhuf <91219279+jdhuf@users.noreply.github.com>
2022-06-19 07:10:56 -0600

    added ng create exercise functionality

--- ngExerciseTracker/src/app/components/home/home.component.ts
+++ ngExerciseTracker/src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { ExerciseService } from './../../services/exercise.service';
 import { Component, OnInit } from '@angular/core';
 import { Exercise } from 'src/app/models/exercise';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,15 +11,71 @@ import { Exercise } from 'src/app/models/exercise';
 export class HomeComponent implements OnInit {
 
 exercises: Exercise [] = [];
+newExercise: Exercise = new Exercise();
+editExercise: null | Exercise = null;
 
-  constructor(private exeriseServ: ExerciseService) { }
+title = "Amazing Exercise Tracker";
+selected: null | Exercise = null;
+
+
+  constructor(
+    private exerciseServ: ExerciseService,
+    private route: ActivatedRoute,
+    private router: Router,
+
+    ) { }
+
+  addExercise(newExercise: Exercise) {
+        this.exerciseServ.create(newExercise).subscribe({
+        next: (newExercise) => {
+        this.newExercise = new Exercise();
+        this.reload();
+        },
+
+        error: (err) => {
+        console.error('HomeComponent.addExercise(): error adding exercise: ');
+        console.error(err);
+        }
+      });
+    }
+
+    updateExercise(exercise: Exercise) {
+      this.exerciseServ.update(exercise).subscribe({
+        next: (result) => {
+
+        this.reload();
+        this.editExercise = null;
+        this.selected = null;
+        },
+
+        error: (err) => {
+          console.error('HomeComponent.addExercise(): error adding exercise: ');
+          console.error(err);
+        }
+      });
+    }
 
   ngOnInit(): void {
     this.reload();
   }
 
+  deleteExercise(id: number): void {
+    this.exerciseServ.destroy(id).subscribe({
+      next: () => {
+        this.reload();
+      },
+      error: (fail) => {
+        console.error('HomeComponent.deleteExercise(): error deleting exercise: ');
+        console.error(fail);
+      }
+
+    });;
+
+
+  }
+
   reload() {
-    this.exeriseServ.index().subscribe({
+    this.exerciseServ.index().subscribe({
       next: (exercises) => {
         this.exercises = exercises;
 
@@ -32,4 +89,22 @@ exercises: Exercise [] = [];
     })
   }
 
+  getTotalNumExercises() {
+    return this.exercises.length;
+  }
+
+  displayExercise(exercise: Exercise) {
+    this.selected = exercise;
+  }
+
+  displayTable(){
+    this.selected = null;
+  }
+
+  setEditExercise() {
+    this.editExercise = Object.assign({}, this.selected);
+  }
+
+
 }
+
--- ngExerciseTracker/src/app/services/exercise.service.ts
+++ ngExerciseTracker/src/app/services/exercise.service.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, throwError } from 'rxjs';
@@ -13,7 +14,10 @@ export class ExerciseService {
 private url = environment.baseUrl + 'api/exercise';
 
 
-  constructor(private http:HttpClient) { }
+  constructor(
+    private http:HttpClient) { }
+
+  // Show All------------------------------------------------------
 
   index(): Observable<Exercise[]> {
     return this.http.get<Exercise[]>(this.url).pipe(
@@ -24,5 +28,63 @@ private url = environment.baseUrl + 'api/exercise';
     );
   }
 
+  // Update One------------------------------------------------------
+
+  update(exercise: Exercise): Observable<Exercise> {
+
+    return this.http.put<Exercise>(this.url + '/' + exercise.id, exercise).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('TodoService.update(): error updating todo: ' + err)
+        );
+      })
+    );
+
+  }
+
+// Show One By Id--------------------------------------------------
+
+showExerciseById(id: number): Observable<Exercise> {
+  return this.http.get<Exercise>(this.url + '/' + id).pipe(
+    catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error('ExerciseService.show(): error showing exercise: ' + err)
+      );
+    })
+  );
+
+}
+
+// Create One--------------------------------------------------------
+
+create(exercise: Exercise) {
+
+
+  console.log(exercise);
+
+return this.http.post<Exercise>(this.url, exercise).pipe(
+  catchError((err: any) => {
+    console.log(err);
+    return throwError(
+      () => new Error('TodoService.create(): error creating todo: ' + err)
+    );
+  })
+);
+}
+
+// Destroy One----------------------------------------------------------
+
+destroy(id: number): Observable<void> {
+  return this.http.delete<void>(this.url +'/'+ id).pipe(
+    catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error('TodoService.index(): error deleting exercise: ' + err)
+      );
+    })
+  );
+}
 
 }
